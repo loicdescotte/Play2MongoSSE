d018044c5fca38c220936804a36fd0f163f96c8b
Loic Descotte <loic.descotte@gmail.com>
2012-11-20 08:00:27 +0100

    public resources

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 import play.api._
 import play.api.mvc._
@@ -22,7 +22,6 @@ import libs.iteratee._
 import libs.EventSource
 import libs.json.JsValue
 
-
 object Application extends Controller with MongoController {
 
   val db = ReactiveMongoPlugin.db
@@ -33,9 +32,8 @@ object Application extends Controller with MongoController {
     mapping(
       "id" -> optional(of[String]),
       "author" -> nonEmptyText,
-      "message" -> nonEmptyText
-    ) (Post.build)(Post.unbuild))
-  
+      "message" -> nonEmptyText)(Post.build)(Post.unbuild))
+
   def index = Action { Home }
 
   def Home = Redirect(routes.Application.list)
@@ -43,45 +41,44 @@ object Application extends Controller with MongoController {
   def list = Action { implicit request =>
     implicit val reader = PostBSONReader
     // Async resturns an AsyncResult from a Promise
-    Async{
+    Async {
       //find all docs
-       val found = collection.find(BSONDocument()).toList
-       found.map { posts =>
-  	     Ok(html.list(posts, postForm))
-       }
+      val found = collection.find(BSONDocument()).toList
+      found.map { posts =>
+        Ok(html.list(posts, postForm))
+      }
     }
   }
-  
+
   implicit val postJsonWrites = new Writes[Post] {
-  def writes(post: Post): JsValue = {
-    Json.obj(
-      "author" -> post.author,
-      "message" -> post.message
-      )
+    def writes(post: Post): JsValue = {
+      Json.obj(
+        "author" -> post.author,
+        "message" -> post.message)
     }
   }
 
   def listJson = Action {
-  implicit val bsonReader = PostBSONReader
-  Async{
-   val found = collection.find(BSONDocument()).toList    
-   found map{
-     posts => Ok(Json.toJson(posts))
-   }
- }
-}
+    implicit val bsonReader = PostBSONReader
+    Async {
+      val found = collection.find(BSONDocument()).toList
+      found map {
+        posts => Ok(Json.toJson(posts))
+      }
+    }
+  }
 
- def searchPage(filter: String) = Action {
+  def searchPage(filter: String) = Action {
     Ok(html.searchResults(filter))
- }
+  }
 
- def search(filter: String) = Action { 
+  def search(filter: String) = Action {
 
     import play.modules.reactivemongo.PlayBsonImplicits._
 
-    Logger.info("filter : " + filter)  
-    val query = QueryBuilder().query(BSONDocument("message" -> BSONRegex(filter, "")))
+    Logger.info("filter : " + filter)
 
+    val query = QueryBuilder().query(BSONDocument("message" -> BSONRegex(filter, "")))
     //query results asynchronous cursor
     val cursor = collection.find[JsValue](query, QueryOpts().tailable.awaitData)
 
@@ -95,44 +92,44 @@ object Application extends Controller with MongoController {
     Ok.stream(dataProducer &> EventSource()).as("text/event-stream")
   }
 
- def edit(id: String) = Action { implicit request =>
+  def edit(id: String) = Action { implicit request =>
     implicit val bsonReader = PostBSONReader
     Async {
       val objectId = new BSONObjectID(id)
       val cursor = collection.find(BSONDocument("_id" -> objectId))
       // promise of option of post!
       cursor.headOption.map {
-          result => result.map {
+        result =>
+          result.map {
             post => Ok(html.edit(id, postForm.fill(post)))
-        }.getOrElse(NotFound)
+          }.getOrElse(NotFound)
       }
     }
- }
-
- def create() = Action { implicit request =>
-  implicit val bsonWriter = PostBSONWriter
- 	//no validation here (just an example :)
-  val post = postForm.bindFromRequest.get
- 	AsyncResult {
-    collection.insert(post).map( _ => Home)
   }
- }
 
- def update(id: String) = Action { implicit request =>
-  postForm.bindFromRequest.fold (
+  def create() = Action { implicit request =>
+    implicit val bsonWriter = PostBSONWriter
+    //no validation here (just an example :)
+    val post = postForm.bindFromRequest.get
+    AsyncResult {
+      collection.insert(post).map(_ => Home)
+    }
+  }
+
+  def update(id: String) = Action { implicit request =>
+    postForm.bindFromRequest.fold(
       //return to edit page with entered values
       errors => Ok(views.html.edit(id, errors)),
       post => AsyncResult {
         val objectId = new BSONObjectID(id)
         val modifier = BSONDocument(
-            "$set" -> BSONDocument(
+          "$set" -> BSONDocument(
             "author" -> BSONString(post.author),
             "message" -> BSONString(post.message)))
-            collection.update(BSONDocument("_id" -> objectId), modifier).map { _ =>
-              Home
+        collection.update(BSONDocument("_id" -> objectId), modifier).map { _ =>
+          Home
         }
-      }
-    )
+      })
   }
 
 }
